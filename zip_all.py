919f6d0fc9ae37f2539c12b86145e91647c97070
ZadoyanMK <konzamir@gmail.com>
2019-05-05 19:30:47 +0300

    Update zip_all.py

--- zip_all.py
+++ zip_all.py
@@ -1,11 +1,13 @@
 import glob
 import sys
 import os
+import argparse
 
-def check(path):
+
+def check(path, symbol, is_index):
     result_file = open('zip_result.txt', 'w+')
     index = 0
-    
+
     for path, subdirs, files in os.walk(path):
         for name in files:
             index += 1
@@ -15,13 +17,22 @@ def check(path):
                 with open(x) as f:
                     file_ctx = f.read()
                     if file_ctx:
-                        result_file.write(f"\n{index}. {x[2:]} ~\n")
+                        if is_index:
+                            result_file.write(f"\n{index}. {x[2:]} {symbol}\n")
+                        else:
+                            result_file.write(f"\n{x[2:]} {symbol}\n")
                         result_file.write(f"{file_ctx}\n")
     result_file.close()
 
 
 if __name__=="__main__":
-    base_path = './'
-    if len(sys.argv) > 1:
-        base_path = sys.argv[1]
-    check(base_path)
+    is_index = False
+    
+    parser = argparse.ArgumentParser(description='Group all file contents to one file.')
+    parser.add_argument('--path', '-p', type=str, help='Base path', default='./')
+    parser.add_argument('--symb', '-s', type=str, help='Symbol after filename at file', default='~')
+    parser.add_argument('--index', '-i', help='Need index to each file?', default=False, action="store_true")
+    args = parser.parse_args()
+
+    check(args.path, args.symb, args.index)
+
